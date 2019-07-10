a50cffd8c9c496c28168ba9e5944eabaf3d73552
workodin.com <hello@workodin.com>
2019-07-10 03:57:12 +0200

    suppression fichier robots.txt

--- private/template/page-section/index.php
+++ private/template/page-section/index.php
@@ -10,6 +10,7 @@
             <p>Travaillez en équipe sur des projets innovants et motivants!</p>
             <p><a href="https://github.com/workodin/workodin.com">Participez au développement du site workodin.com sur GitHub</a></p>
         </article>
+
         <article>
             <h3>Vous avez une question ?</h3>
             <h4><a href="/contact">Contactez-nous !</a></h4>
@@ -17,25 +18,22 @@
                 <img src="/assets/images/workodin-team2.jpg" alt="workodin team">
             </figure>
         </article>
+        
         <article>
             <h3>Suivez le projet</h3>
             <h4>inscrivez-vous à la newsletter</h4>
-            <div class="row">
-                <div class="col col40">
-                    <form id="form-newsletter" action="#form-newsletter" method="POST">
-                        <label for="form-nom">nom</label>
-                        <input id="form-nom" type="text" name="name" required placeholder="votre nom">
-                        <label for="form-email">email</label>
-                        <input id="form-email" type="email" name="email" required placeholder="votre email">
-                        <button type="submit">inscription</button>
-                        <input type="hidden" name="formKey" value="Newsletter">
-                        <input type="hidden" name="formTag" value="<?php $form->show("formKeyPublic") ?>">
-                        <div class="feedback">
-                            <?php $form->process("Newsletter") ?>
-                        </div>
-                    </form>
-                </div>
-            </div>
+                <form id="form-newsletter" action="#form-newsletter" method="POST">
+                    <label for="form-nom">nom</label>
+                    <input id="form-nom" type="text" name="name" required placeholder="votre nom">
+                    <label for="form-email">email</label>
+                    <input id="form-email" type="email" name="email" required placeholder="votre email">
+                    <button type="submit">inscription</button>
+                    <input type="hidden" name="formKey" value="Newsletter">
+                    <input type="hidden" name="formTag" value="<?php $form->show("formKeyPublic") ?>">
+                    <div class="feedback">
+                        <?php $form->process("Newsletter") ?>
+                    </div>
+                </form>
         </article>
     </div>
 </section>
